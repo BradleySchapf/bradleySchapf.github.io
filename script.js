e62c44bfd768cf1a6b8b8d6a8b67617bb66dd1ee
BradleySchapf <102550421+BradleySchapf@users.noreply.github.com>
2023-12-13 20:11:06 +0100

    javascript validation

---
+++ script.js
@@ -0,0 +1,91 @@
+
+function validate(e) {
+    //  Hides all error elements on the page
+    hideAllErrors();
+
+    //  Determine if the form has errors
+    if (formHasErrors()) {
+        //  Prevents the form from submitting
+        e.preventDefault();
+        return false;
+    }
+
+    return true;
+}
+
+function formHasErrors() {
+    // Code below here
+    let errorFlag = false;
+
+    let requiredFields = ["name", "phone", "email", "comments"];
+
+    for(let i = 0; i < requiredFields.length; i++) {
+        let textField = document.getElementById(requiredFields[i]);
+        console.log(textField);
+        if(!formFieldHasInput(textField)) {
+            document.getElementById(requiredFields[i] + "_error").style.display = "block";
+
+            if(!errorFlag) {
+                textField.focus();
+                textField.select();
+            }
+
+            //Raise the error flag
+            errorFlag = true;
+        }
+    }
+
+    return errorFlag;
+}
+
+function formFieldHasInput(fieldElement) {
+    // Check if the text field has a value
+    if (fieldElement.value == null || fieldElement.value.trim() == "") {
+        // Invalid entry
+        return false;
+    }
+
+    // Valid entry
+    return true;
+}
+
+function hideAllErrors() {
+    //Get an array of the error fields
+    let errorFields = document.getElementsByClassName("error");
+    for(let i=0;i < errorFields.length; i++) {
+        errorFields[i].style.display = "none";
+    }
+}
+
+function resetForm(e) {
+    // Confirm that the user wants to reset the form.
+    if (confirm('Clear survey?')) {
+        // Ensure all error fields are hidden
+        hideAllErrors();
+
+        // Set focus to the first text field on the page
+        document.getElementById("name").focus();
+
+        // When using onReset="resetForm()" in markup, returning true will allow
+        // the form to reset
+        return true;
+    }
+
+    // Prevents the form from resetting
+    e.preventDefault();
+
+    // When using onReset="resetForm()" in markup, returning false would prevent
+    // the form from resetting
+    return false;
+}
+
+function load() {
+    document.getElementById("contactForm").reset();
+
+    document.getElementById("contactForm").addEventListener("submit", validate) 
+    document.getElementById("contactForm").addEventListener("reset", resetForm)
+}
+
+
+// Add the event listener for the document load
+document.addEventListener("DOMContentLoaded", load);
